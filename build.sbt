8e9e5069f6a631069e0c11801c455dbaeed47e4c
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-10-13 15:54:35 +0900

    feat: TCP接続を別host:portに転送する機能を追加

--- build.sbt
+++ build.sbt
@@ -4,5 +4,8 @@ lazy val root = (project in file(".")).settings(
       scalaVersion := "2.12.7",
     )),
   name := "TCP-Forwarding",
+  libraryDependencies ++= Seq(
+    "com.typesafe.akka" %% "akka-stream" % "2.5.17",
+  ),
   scalafmtOnCompile := true,
 )
---
+++ src/main/scala/TcpForwarding.scala
@@ -0,0 +1,29 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Tcp.{IncomingConnection, ServerBinding}
+import akka.stream.scaladsl.{Source, Tcp}
+
+import scala.concurrent.Future
+
+object TcpForwarding extends App {
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val config = system.settings.config.getConfig("tcp-forwarding")
+
+  val bindHost = config.getString("bind.host")
+  val bindPort = config.getInt("bind.port")
+
+  val targetHost = config.getString("target.host")
+  val targetPort = config.getInt("target.port")
+
+  val connections: Source[IncomingConnection, Future[ServerBinding]] =
+    Tcp().bind(bindHost, bindPort)
+
+  println(s"Started.\n$bindHost:$bindPort -> $targetHost:$targetPort")
+
+  connections runForeach { connection ⇒
+    val outgoingConnection = Tcp().outgoingConnection(targetHost, targetPort)
+    connection.handleWith(outgoingConnection)
+  }
+}
