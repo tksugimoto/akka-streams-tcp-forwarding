49d86afd84dcbd2dbbb6c5704681fe8775e05405
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-10-16 00:41:52 +0900

    feat: HTTP リクエストの 1行目の take/drop flow を作成

---
+++ src/main/scala/HttpLineSeparator.scala
@@ -0,0 +1,19 @@
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
+import akka.util.ByteString
+
+object HttpLineSeparator {
+  private val CR = ByteString("\r")
+
+  // TODO: 効率化　(1Byteずつにsplitして判定するのは非効率)
+  private val splitByteString = Flow[ByteString].mapConcat(_.map(ByteString(_)))
+
+  val takeFirstLine: Flow[ByteString, ByteString, NotUsed] = Flow[ByteString]
+    .via(splitByteString)
+    .takeWhile(_ != CR)
+    .fold(ByteString.empty)(_ ++ _)
+
+  val dropFirstLine: Flow[ByteString, ByteString, NotUsed] = Flow[ByteString]
+    .via(splitByteString)
+    .dropWhile(_ != CR)
+}
