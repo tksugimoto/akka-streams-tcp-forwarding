9ed209c233ffe026d4f27e616a7177785bc5646a
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-11-05 00:03:26 +0900

    refactor: HTTPリクエストの 1行目の take/drop 処理を効率化

--- src/main/scala/HttpLineSeparator.scala
+++ src/main/scala/HttpLineSeparator.scala
@@ -3,18 +3,39 @@ import akka.stream.scaladsl.Flow
 import akka.util.ByteString
 
 object HttpLineSeparator {
-  private val CR = ByteString("\r")
-
-  // TODO: 効率化　(1Byteずつにsplitして判定するのは非効率)
-  private val splitByteString = Flow[ByteString].mapConcat(_.map(ByteString(_)))
+  private val CR: Byte = ByteString("\r")(0)
 
   val takeFirstLine: Flow[ByteString, ByteString, NotUsed] = Flow[ByteString]
-    .via(splitByteString)
-    .takeWhile(_ != CR)
+    .statefulMapConcat { () =>
+      var inFirstLine = true
+      input =>
+        if (inFirstLine) {
+          if (input.contains(CR)) {
+            inFirstLine = false
+            Vector(input.takeWhile(_ != CR))
+          } else {
+            Vector(input)
+          }
+        } else {
+          Vector.empty
+        }
+    }
     .fold(ByteString.empty)(_ ++ _)
     .filterNot(_.isEmpty)
 
   val dropFirstLine: Flow[ByteString, ByteString, NotUsed] = Flow[ByteString]
-    .via(splitByteString)
-    .dropWhile(_ != CR)
+    .statefulMapConcat { () =>
+      var inFirstLine = true
+      input =>
+        if (inFirstLine) {
+          if (input.contains(CR)) {
+            inFirstLine = false
+            Vector(input.dropWhile(_ != CR))
+          } else {
+            Vector.empty
+          }
+        } else {
+          Vector(input)
+        }
+    }
 }
